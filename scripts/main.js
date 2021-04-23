ca400f824ebfc376f32067c0b6218ef2034c4587
ugomazocorodrigues <47155303+ugomazocorodrigues@users.noreply.github.com>
2021-04-23 08:21:50 -0300

    Melhorias

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,13 @@ function myFunction() {
   }
 }
 
-@import "../node_modules/bootstrap/scss/bootstrap";
+// @import "../node_modules/bootstrap/scss/bootstrap";
 
-$accordion-border-color: #FFF; 
+// function showPosition(position) {
+//   var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+//   var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=
+//   "+latlon+" & zoom=14 & size=400x300 & sensor=false & key=YOUR_KEY";
+
+//   document.getElementById("mapholder").innerHTML = "<img src='" + img_url + "'>";
+// }
